efd25dc1a0761a61eadc1fd810392d7a79d0d021
MAnwar <MAnwar@aas.com.sa>
2025-10-08 02:33:26 +0300

    resource customization

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,25 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function getRouteKeyName() {
+        return "name";
+    }
+    
+    #[Scope]
+    public function verified()
+    {
+        return $this->whereNotNull('email_verified_at');
+    }
 
     /**
      * The attributes that are mass assignable.
